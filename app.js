819421e3512220f79587ca799b0e9be9ad9e028b
varunmalhotraminjar <ec2-user@ip-172-31-0-247.ec2.internal>
2016-07-28 12:28:55 +0000

    push mae

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Hello World");
+  res.send("Jenkins Build Pipeline");
 });
 
 app.listen(4000);
