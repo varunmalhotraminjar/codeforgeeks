064e4193fe46f2d954bc65e16b54356b664f04df
varunmalhotraminjar <ec2-user@ip-172-31-0-247.ec2.internal>
2016-07-28 12:40:53 +0000

    recently

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
-  res.send("Jenkins Build Pipeline");
+  res.send("Jenkins Build Pipeline using GITHUB- 2");
 });
 
 app.listen(4000);
